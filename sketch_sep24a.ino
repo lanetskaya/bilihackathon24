4ea9a3d60f32a0d9b349544cadd0225915a3de48
lanetskaya <81872500+lanetskaya@users.noreply.github.com>
2024-10-09 14:11:00 +0200

    Add files via upload

--- sketch_sep24a.ino
+++ sketch_sep24a.ino
@@ -7,15 +7,18 @@
 
 MFRC522 rfid(SS_PIN, RST_PIN);      // Create MFRC522 instance.
 Servo myservo;
+
 byte firstUID[10];  // Variable to store the first UID (up to 10 bytes)
-bool isFirstTagSet = false; // Flag to indicate if the first tag has been stored
+byte secondUID[10]; // Variable to store the second UID (up to 10 bytes)
+bool isFirstTagSet = false;  // Flag to indicate if the first tag has been stored
+bool isSecondTagSet = false; // Flag to indicate if the second tag has been stored
 int pos = 0;
 
 void setup() {
   Serial.begin(9600);
   SPI.begin(); // init SPI bus
   rfid.PCD_Init(); // init MFRC522
-  myservo.attach(9); 
+  myservo.attach(5); 
   Serial.println("Tap RFID/NFC Tag on reader for initial setup.");
 }
 
@@ -40,23 +43,34 @@ void loop() {
           Serial.print(firstUID[i], HEX);
         }
         Serial.println();
-        Serial.println("First tag has been stored. All subsequent tags will be compared to this tag.");
+        Serial.println("First tag has been stored. Tap another tag to store the second tag.");
+
+      } else if (!isSecondTagSet) {
+        // Store the second tag UID
+        for (int i = 0; i < rfid.uid.size; i++) {
+          secondUID[i] = rfid.uid.uidByte[i];
+        }
+        isSecondTagSet = true; // Mark the second tag as stored
+
+        // Print the stored second UID
+        Serial.print("Second UID stored:");
+        for (int i = 0; i < rfid.uid.size; i++) {
+          Serial.print(secondUID[i] < 0x10 ? " 0" : " ");
+          Serial.print(secondUID[i], HEX);
+        }
+        Serial.println();
+        Serial.println("Second tag has been stored. All subsequent tags will be compared to the first two tags.");
 
       } else {
-        // Compare the current tag with the first stored UID
+        // Compare the current tag with the first and second stored UIDs
         if (compareUIDs(rfid.uid.uidByte, firstUID, rfid.uid.size)) {
-          Serial.println("The current tag matches the initial tag.");
-          for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-          // in steps of 1 degree
-            myservo.write(pos);              // tell servo to go to position in variable 'pos'
-            delay(15);                       // waits 15ms for the servo to reach the position
-          }
-         /*  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
-          delay(15);                       // waits 15ms for the servo to reach the position
-          } */
+          Serial.println("The current tag matches the first tag.");
+          moveServo(); // Move the motor if the first tag is matched
+        } else if (compareUIDs(rfid.uid.uidByte, secondUID, rfid.uid.size)) {
+          Serial.println("The current tag matches the second tag.");
+          moveServo(); // Move the motor if the second tag is matched
         } else {
-          Serial.println("The current tag does not match the initial tag.");
+          Serial.println("The current tag does not match either stored tag.");
         }
 
         // Print the current UID
@@ -82,4 +96,17 @@ bool compareUIDs(byte *uid1, byte *uid2, byte size) {
     }
   }
   return true;
+}
+
+// Function to move the servo motor
+void moveServo() {
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(5000);                       // Delay for 5 seconds (you can adjust)
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 90 degrees back to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
